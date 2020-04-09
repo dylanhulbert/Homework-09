d04b36e2a0e159bb5a2da1c8a9409a47bc295802
dylan@dylanhulbert.com <dylan@dylanhulbert.com>
2020-04-09 00:21:21 -0700

    access index.public/index.html

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
 app.listen(PORT, function() {
 	console.log("Listening on " + PORT);
 });
